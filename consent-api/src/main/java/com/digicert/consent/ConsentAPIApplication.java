68dbffcd046b9757b9613313008679cd586ef12a
kirthanmp <kirthan.puttegowda@digicert.com>
2024-05-28 11:41:23 +0200

    migrations fix

--- consent-api/src/main/java/com/digicert/consent/ConsentAPIApplication.java
+++ consent-api/src/main/java/com/digicert/consent/ConsentAPIApplication.java
@@ -1,7 +1,11 @@
 package com.digicert.consent;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConsentAPIApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConsentAPIApplication.class, args);
     }
 }
