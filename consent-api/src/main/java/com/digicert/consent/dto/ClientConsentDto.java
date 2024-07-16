9666ba675f374a3eecc427c62292f510ac286cc1
kirthanmp <kirthan.puttegowda@digicert.com>
2024-07-16 11:50:55 +0200

    development check in

--- consent-api/src/main/java/com/digicert/consent/dto/ClientConsentDto.java
+++ consent-api/src/main/java/com/digicert/consent/dto/ClientConsentDto.java
@@ -2,9 +2,13 @@ package com.digicert.consent.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ClientConsentDto {
     private String productName;
+    private String template;
     private String individualId;
     private String outputReason;
+    private List<ClientConsentMetadataDto> metadata;
 }
---
+++ consent-api/src/main/java/com/digicert/consent/enums/OptOutReason.java
@@ -0,0 +1,6 @@
+package com.digicert.consent.enums;
+
+public enum OptOutReason {
+    AGREE,
+    DISAGREE
+}
