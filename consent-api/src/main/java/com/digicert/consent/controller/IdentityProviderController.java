3cf939a6acdb5ffeda22e72a85e589d585503283
desiree.eveleigh <desiree.eveleigh@digicert.com>
2024-08-05 16:24:15 +0200

    reverted

--- consent-api/src/main/java/com/digicert/consent/controller/IdentityProviderController.java
+++ consent-api/src/main/java/com/digicert/consent/controller/IdentityProviderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("identity-provider")
+@RequestMapping("consent/identity-provider")
 @AllArgsConstructor
 public class IdentityProviderController {
 
